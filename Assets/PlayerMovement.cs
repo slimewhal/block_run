7167d167fd7e3a1dbda1c1eee81b40fa46d4c9db
jeremiah.nunn <jeremy@sketchyventures.com>
2019-01-04 13:08:39 -0700

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+	public float forwardSpeed = 10f;
+	public float horzSpeed = 3f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		var vel = Vector3.forward * forwardSpeed;
+		vel.y = rb.velocity.y;
+        if (Input.GetKey(KeyCode.A))
+        {
+			vel += Vector3.left * horzSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+			vel += Vector3.right * horzSpeed;
+        }
+		rb.velocity = vel;
+    }
+}
---
+++ Assets/playercollision.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class playercollision : MonoBehaviour {
+public PlayerMovement movement; 
+void OnCollisionEnter(Collision CollisionInfo){
+	if(CollisionInfo.collider.tag == "obstical"){
+		movement.enabled = false;
+}
+
+}
+}
